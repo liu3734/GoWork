b197929d73e69bd25812a706e8eb0884299f0608
liuying3 <LIU4862ying>
2021-11-01 01:52:59 +0800

    feat 伪代码提交

---
+++ week2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gpmgo/gopm/modules/errors"
+)
+
+func main() {
+	userId := 10
+	username, err := QueryUserName(userId)
+	if err == sql.ErrNoRows {
+		fmt.Println(err)
+	}
+	fmt.Println(username)
+}
+func QueryUserName(id int) (string, error) {
+	userName := ""
+	err := db.QueryRow("select user_name from users where id=?", id).Scan(&userName)
+	return userName, errors.Wrap(err, "error when get user name")
+}
